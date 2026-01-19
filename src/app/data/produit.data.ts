cc1984c541e2bbabc14c9a17c1853161462b0ba1
ALGA <algassimoub@ept.sn>
2026-01-19 13:32:25 +0000

    ajout de produits

---
+++ src/app/data/produit.data.ts
@@ -0,0 +1,35 @@
+import { Produit } from "../models/produit.model";
+
+export const PRODUITS: Produit[] = [
+            {
+            id :1 ,
+            nom: "pomme",
+            prix: 300,
+            quantite: 5,
+            },
+            {
+            id :2 ,
+            nom: "orange",
+            prix: 200,
+            quantite: 4,
+            },
+            {
+            id :3,
+            nom: "mangue",
+            prix: 150,
+            quantite: 6,
+            },
+            {
+            id :4 ,
+            nom: "annanas",
+            prix: 800,
+            quantite: 2,
+            },
+            {
+            id :5,
+            nom: "pasteque",
+            prix: 1000,
+            quantite: 13,
+            },
+
+]
